fd2c1265dcfe9dee47261ef7f9a5984d1e7e3eab
“xixn2” <ssbkh0802@naver.com>
2024-10-23 09:47:22 +0900

    ✨ :: [#10] Add Details Top View Publishing

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -25,7 +25,7 @@ let targets: [Target] = [
         bundleId: "\(env.organizationName).\(env.name)",
         deploymentTargets: env.deploymentTargets,
         infoPlist: .file(path: "Support/Info.plist"),
-        sources: ["Sources/**"],
+        sources: ["Sources**"],
         resources: ["Resources/**"],
         scripts: scripts,
         dependencies: [
