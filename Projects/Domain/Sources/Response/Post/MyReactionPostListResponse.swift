bd3565587ad0ad17d119879d7b615b8ecd218bc5
“xixn2” <ssbkh0802@naver.com>
2024-12-23 07:59:42 +0900

    ✨ :: [#22] MyReactionPostListResponse 추가

---
+++ Projects/Domain/Sources/Response/Post/MyReactionPostListResponse.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+public struct MyReactionPostListResponse: Codable {
+    public let id: Int
+    public let author: ReactionAuthorInfo
+    public let title: String
+    public let imageUrl: [String]
+    public let location: String
+    public let tagList: [MyReactionPostListTaggedUser]
+    public let emojiList: MyReactionPostListEmojiCounts
+    public let createdTime: String
+}
+
+public struct ReactionAuthorInfo: Codable {
+    public let id: Int
+    public let name: String
+    public let grade: Int
+}
+
+public struct MyReactionPostListTaggedUser: Codable {
+    public let name: String
+    public let id: Int
+}
+
+public struct MyReactionPostListEmojiCounts: Codable {
+    public let heartCount: Int
+    public let congCount: Int
+    public let thumbsCount: Int
+    public let thinkCount: Int
+    public let poopCount: Int
+    public let chinaCount: Int
+}
