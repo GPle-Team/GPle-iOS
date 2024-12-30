b70eb3bbd062153b941096d2d1667bb2e1963a5f
kimkyumbi <kimgyeombi@naver.com>
2024-12-30 10:18:36 +0900

    수정

--- Projects/App/Sources/Application/GPleApp.swift
+++ Projects/App/Sources/Application/GPleApp.swift
@@ -1,8 +1,17 @@
 import SwiftUI
-import GoogleSignIn
+import FirebaseCore
+import Firebase
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct GoogleSignInProjectApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var viewModel: LoginViewModel = LoginViewModel()
     @StateObject var userInfoViewModel: UserInfoViewModel = UserInfoViewModel()
     
