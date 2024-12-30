452f1a6a5f1423788ffa46d993d9ab63269cf7d4
“xixn2” <ssbkh0802@naver.com>
2024-12-30 10:30:12 +0900

    🔥 :: [#40] Firebase 삭제

--- Projects/App/Sources/Application/GPleApp.swift
+++ Projects/App/Sources/Application/GPleApp.swift
@@ -1,17 +1,7 @@
 import SwiftUI
-import FirebaseCore
-import Firebase
-
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
-        return true
-    }
-}
 
 @main
 struct GoogleSignInProjectApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var viewModel: LoginViewModel = LoginViewModel()
     @StateObject var userInfoViewModel: UserInfoViewModel = UserInfoViewModel()
     
